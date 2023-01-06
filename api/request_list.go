b9859a405012dc469a6bb0d591840b7c68ad8ba5
caroll kla <caroll.kla@gmail.com>
2023-01-06 02:46:48 +0100

    /v2/requestlist endpoint clean sql syntax

--- api/request_list.go
+++ api/request_list.go
@@ -9,7 +9,7 @@ import (
 
 func (s Service) RequestListStatus(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Status string `db:"''" json:"''"`
+		Status string 
 	}
 
 	err := read(r, &input)
